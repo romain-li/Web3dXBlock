38cf25109633fb814488aeacaa0bedbde97558eb
lixue <romain_li@163.com>
2015-07-26 17:39:02 +0800

    Add event for cancel button.

--- web3d/static/js/src/web3d_studio.js
+++ web3d/static/js/src/web3d_studio.js
@@ -1,15 +1,17 @@
 /* Javascript for Web3dXBlock. */
 function Web3dXBlockStudio(runtime, element) {
-    $(function ($) {
-        $('.save-button', element).on('click', function () {
-            var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
+    $('.save-button', element).on('click', function () {
+        var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
 
-            $.post(handlerUrl, JSON.stringify({
-                obj: $('input#obj', element).val(),
-                mtl: $('input#mtl', element).val()
-            })).done(function (response) {
-                window.location.reload(false);
-            });
+        $.post(handlerUrl, JSON.stringify({
+            obj: $('input#obj', element).val(),
+            mtl: $('input#mtl', element).val()
+        })).done(function () {
+            window.location.reload(false);
         });
+    });
+
+    $('.cancel-button', element).on('click', function () {
+        runtime.notify('cancel', {});
     });
 }
